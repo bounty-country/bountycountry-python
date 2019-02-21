02837a630eb44f52a325d0288dbd444b5fb3b64d
Bounty Country <47579576+BountyCountry@users.noreply.github.com>
2019-02-22 10:18:06 +1100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='bountycountry',
-      version='1.3.2',
+      version='1.4.0',
       url='https://github.com/bounty-country/bountycountry-python',
       license='MIT',
       author='Bounty Country',
